3d458e6ab724f29307fd233fbc8158abc29c3a71
Nikhil shinde <nikhilsubsun123@gmail.com>
2026-06-02 20:14:32 +0530

    enable returning user tracking

--- components/providers/PostHogProvider.tsx
+++ components/providers/PostHogProvider.tsx
@@ -6,7 +6,7 @@ import { PostHogProvider as CSPostHogProvider } from "posthog-js/react";
 if (typeof window !== "undefined") {
   posthog.init("phc_vwS2QJNp7U9gAS3x5dGpcnVbFxXpiiexsKsGVnoQ3p5P", {
     api_host: "https://us.i.posthog.com",
-    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    person_profiles: "always", // Track anonymous users across sessions
   });
 }
 
